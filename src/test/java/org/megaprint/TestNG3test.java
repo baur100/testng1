480563531d35e273ee09ab361b52db5b4544d089
smarot <smarot10@mail.ru>
2017-04-29 22:01:00 -0400

    TestNG1 Lesson archive

---
+++ src/test/java/org/megaprint/TestNG3test.java
@@ -0,0 +1,23 @@
+package org.megaprint;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Baurz on 4/22/2017.
+ */
+public class TestNG3test {
+    @Test
+    public void test1() {
+        System.out.println("NEW-TEST1 inside");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("NEW-TEST2 inside");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("NEW-TEST3 inside");
+    }
+}
---
+++ src/test/java/org/megaprint/TestNG4test.java
@@ -0,0 +1,23 @@
+package org.megaprint;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Baurz on 4/22/2017.
+ */
+public class TestNG4test {
+    @Test
+    public void test1() {
+        System.out.println("2-NEW-TEST1 inside");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("2-NEW-TEST2 inside");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("2-NEW-TEST3 inside");
+    }
+}
